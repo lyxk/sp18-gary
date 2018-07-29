e5093d4ccf30e18455a45c4e4c331475af629877
lyxk <2917971313@qq.com>
2018-07-28 17:34:35 -0700

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,13 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		while (x < 10) {
+			System.out.println(x);
+			x = x + 1;
+		}
+	}
+}
+
+/*
+Types are verified before the code even runs
+*/
